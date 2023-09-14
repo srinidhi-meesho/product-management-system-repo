4f265e4ffe0ac1d3ddb21d38e909dd2a55a17534
srinidhi-meesho <srinidhi.nadendla@meesho.com>
2023-09-15 01:09:31 +0530

    constants_file_changes

--- src/main/java/com/example/productmanagementsystemrepo/Constants/Constants.java
+++ src/main/java/com/example/productmanagementsystemrepo/Constants/Constants.java
@@ -3,12 +3,12 @@ package com.example.productmanagementsystemrepo.Constants;
 
 
 public class Constants {
-   public class SupplierPayment {
+   public static class SupplierPayment {
        public static final String URL = "${supplier-payment.url}";
        public static final String NAME = "supplierPayment";
    }
 
-    public class Kafka {
+    public static class Kafka {
         public static final String GROUP_ID = "${spring.kafka.consumer.group-id}";
 
         public static final String TOPICS = "${spring.kafka.consumer.topics}";
